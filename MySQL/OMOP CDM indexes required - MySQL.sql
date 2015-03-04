a27e37d6c9e6d4593dcef0d979911e062d50454c
Colin Erdman <cerdman@adverseevents.com>
2015-03-04 07:14:37 -0800

    fix typo.

--- MySQL/OMOP CDM indexes required - MySQL.sql
+++ MySQL/OMOP CDM indexes required - MySQL.sql
@@ -45,50 +45,50 @@ Standardized vocabulary
 ************************/
 
 CREATE UNIQUE INDEX  idx_concept_concept_id  ON  concept  (concept_id ASC);
-CLUSTER  concept  USING  idx_concept_concept_id ;
+
 CREATE INDEX idx_concept_code ON concept (concept_code ASC);
 CREATE INDEX idx_concept_vocabluary_id ON concept (vocabulary_id ASC);
 CREATE INDEX idx_concept_domain_id ON concept (domain_id ASC);
 CREATE INDEX idx_concept_class_id ON concept (concept_class_id ASC);
 
 CREATE UNIQUE INDEX  idx_vocabulary_vocabulary_id  ON  vocabulary  (vocabulary_id ASC);
-CLUSTER  vocabulary  USING  idx_vocabulary_vocabulary_id ;
+
 
 CREATE UNIQUE INDEX  idx_domain_domain_id  ON  domain  (domain_id ASC);
-CLUSTER  domain  USING  idx_domain_domain_id ;
+
 
 CREATE UNIQUE INDEX  idx_concept_class_class_id  ON  concept_class  (concept_class_id ASC);
-CLUSTER  concept_class  USING  idx_concept_class_class_id ;
+
 
 CREATE INDEX idx_concept_relationship_id_1 ON concept_relationship (concept_id_1 ASC); 
 CREATE INDEX idx_concept_relationship_id_2 ON concept_relationship (concept_id_2 ASC); 
 CREATE INDEX idx_concept_relationship_id_3 ON concept_relationship (relationship_id ASC); 
 
 CREATE UNIQUE INDEX  idx_relationship_rel_id  ON  relationship  (relationship_id ASC);
-CLUSTER  relationship  USING  idx_relationship_rel_id ;
+
 
 CREATE INDEX  idx_concept_synonym_id  ON  concept_synonym  (concept_id ASC);
-CLUSTER  concept_synonym  USING  idx_concept_synonym_id ;
+
 
 CREATE INDEX  idx_concept_ancestor_id_1  ON  concept_ancestor  (ancestor_concept_id ASC);
-CLUSTER  concept_ancestor  USING  idx_concept_ancestor_id_1 ;
+
 CREATE INDEX idx_concept_ancestor_id_2 ON concept_ancestor (descendant_concept_id ASC);
 
 CREATE INDEX  idx_source_to_concept_map_id_3  ON  source_to_concept_map  (target_concept_id ASC);
-CLUSTER  source_to_concept_map  USING  idx_source_to_concept_map_id_3 ;
+
 CREATE INDEX idx_source_to_concept_map_id_1 ON source_to_concept_map (source_vocabulary_id ASC);
 CREATE INDEX idx_source_to_concept_map_id_2 ON source_to_concept_map (target_vocabulary_id ASC);
 CREATE INDEX idx_source_to_concept_map_code ON source_to_concept_map (source_code ASC);
 
 CREATE INDEX  idx_drug_strength_id_1  ON  drug_strength  (drug_concept_id ASC);
-CLUSTER  drug_strength  USING  idx_drug_strength_id_1 ;
+
 CREATE INDEX idx_drug_strength_id_2 ON drug_strength (ingredient_concept_id ASC);
 
 CREATE INDEX  idx_cohort_definition_id  ON  cohort_definition  (cohort_definition_id ASC);
-CLUSTER  cohort_definition  USING  idx_cohort_definition_id ;
+
 
 CREATE INDEX  idx_attribute_definition_id  ON  attribute_definition  (attribute_definition_id ASC);
-CLUSTER  attribute_definition  USING  idx_attribute_definition_id ;
+
 
 
 /**************************
@@ -108,54 +108,54 @@ Standardized clinical data
 ************************/
 
 CREATE UNIQUE INDEX  idx_person_id  ON  person  (person_id ASC);
-CLUSTER  person  USING  idx_person_id ;
+
 
 CREATE INDEX  idx_observation_period_id  ON  observation_period  (person_id ASC);
-CLUSTER  observation_period  USING  idx_observation_period_id ;
+
 
 CREATE INDEX  idx_specimen_person_id  ON  specimen  (person_id ASC);
-CLUSTER  specimen  USING  idx_specimen_person_id ;
+
 CREATE INDEX idx_specimen_concept_id ON specimen (specimen_concept_id ASC);
 
 CREATE INDEX  idx_death_person_id  ON  death  (person_id ASC);
-CLUSTER  death  USING  idx_death_person_id ;
+
 
 CREATE INDEX  idx_visit_person_id  ON  visit_occurrence  (person_id ASC);
-CLUSTER  visit_occurrence  USING  idx_visit_person_id ;
+
 CREATE INDEX idx_visit_concept_id ON visit_occurrence (visit_concept_id ASC);
 
 CREATE INDEX  idx_procedure_person_id  ON  procedure_occurrence  (person_id ASC);
-CLUSTER  procedure_occurrence  USING  idx_procedure_person_id ;
+
 CREATE INDEX idx_procedure_concept_id ON procedure_occurrence (procedure_concept_id ASC);
 CREATE INDEX idx_procedure_visit_id ON procedure_occurrence (visit_occurrence_id ASC);
 
 CREATE INDEX  idx_drug_person_id  ON  drug_exposure  (person_id ASC);
-CLUSTER  drug_exposure  USING  idx_drug_person_id ;
+
 CREATE INDEX idx_drug_concept_id ON drug_exposure (drug_concept_id ASC);
 CREATE INDEX idx_drug_visit_id ON drug_exposure (visit_occurrence_id ASC);
 
 CREATE INDEX  idx_device_person_id  ON  device_exposure  (person_id ASC);
-CLUSTER  device_exposure  USING  idx_device_person_id ;
+
 CREATE INDEX idx_device_concept_id ON device_exposure (device_concept_id ASC);
 CREATE INDEX idx_device_visit_id ON device_exposure (visit_occurrence_id ASC);
 
 CREATE INDEX  idx_condition_person_id  ON  condition_occurrence  (person_id ASC);
-CLUSTER  condition_occurrence  USING  idx_condition_person_id ;
+
 CREATE INDEX idx_condition_concept_id ON condition_occurrence (condition_concept_id ASC);
 CREATE INDEX idx_condition_visit_id ON condition_occurrence (visit_occurrence_id ASC);
 
 CREATE INDEX  idx_measurement_person_id  ON  measurement  (person_id ASC);
-CLUSTER  measurement  USING  idx_measurement_person_id ;
+
 CREATE INDEX idx_measurement_concept_id ON measurement (measurement_concept_id ASC);
 CREATE INDEX idx_measurement_visit_id ON measurement (visit_occurrence_id ASC);
 
 CREATE INDEX  idx_note_person_id  ON  note  (person_id ASC);
-CLUSTER  note  USING  idx_note_person_id ;
+
 CREATE INDEX idx_note_concept_id ON note (note_type_concept_id ASC);
 CREATE INDEX idx_note_visit_id ON note (visit_occurrence_id ASC);
 
 CREATE INDEX  idx_observation_person_id  ON  observation  (person_id ASC);
-CLUSTER  observation  USING  idx_observation_person_id ;
+
 CREATE INDEX idx_observation_concept_id ON observation (observation_concept_id ASC);
 CREATE INDEX idx_observation_visit_id ON observation (visit_occurrence_id ASC);
 
@@ -182,7 +182,7 @@ Standardized health economics
 ************************/
 
 CREATE INDEX  idx_period_person_id  ON  payer_plan_period  (person_id ASC);
-CLUSTER  payer_plan_period  USING  idx_period_person_id ;
+
 
 
 
@@ -202,14 +202,14 @@ CREATE INDEX idx_ca_subject_id ON cohort_attribute (subject_id ASC);
 CREATE INDEX idx_ca_definition_id ON cohort_attribute (cohort_definition_id ASC);
 
 CREATE INDEX  idx_drug_era_person_id  ON  drug_era  (person_id ASC);
-CLUSTER  drug_era  USING  idx_drug_era_person_id ;
+
 CREATE INDEX idx_drug_era_concept_id ON drug_era (drug_concept_id ASC);
 
 CREATE INDEX  idx_dose_era_person_id  ON  dose_era  (person_id ASC);
-CLUSTER  dose_era  USING  idx_dose_era_person_id ;
+
 CREATE INDEX idx_dose_era_concept_id ON dose_era (drug_concept_id ASC);
 
 CREATE INDEX  idx_condition_era_person_id  ON  condition_era  (person_id ASC);
-CLUSTER  condition_era  USING  idx_condition_era_person_id ;
+
 CREATE INDEX idx_condition_era_concept_id ON condition_era (condition_concept_id ASC);
 
