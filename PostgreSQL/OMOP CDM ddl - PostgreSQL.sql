230a1b09611995742f8592c975e389b1f6c4ab52
Karthik Natarajan <kan7003@knatarajan.cpmc.columbia.edu>
2017-01-12 16:10:00 -0500

    Made changes to adhere to the new CDM changes for version 5.1 for Oracle, Postgres, and SQL Server. Added death_datetime to the death table

--- PostgreSQL/OMOP CDM ddl - PostgreSQL.sql
+++ PostgreSQL/OMOP CDM ddl - PostgreSQL.sql
@@ -289,7 +289,8 @@ CREATE TABLE death
     ( 
      person_id							INTEGER			NOT NULL , 
      death_date							DATE			NOT NULL , 
-     death_type_concept_id				INTEGER			NOT NULL , 
+     death_datetime							TIMESTAMP			NULL ,
+     death_type_concept_id				INTEGER			NOT NULL ,
      cause_concept_id					INTEGER			NULL , 
      cause_source_value					VARCHAR(50)		NULL,
 	 cause_source_concept_id			INTEGER			NULL
--- Sql Server/OMOP CDM ddl - SQL Server.sql
+++ Sql Server/OMOP CDM ddl - SQL Server.sql
@@ -289,6 +289,7 @@ CREATE TABLE death
 (
   person_id							INTEGER			NOT NULL ,
   death_date							DATE			NOT NULL ,
+  death_datetime							DATETIME2			NULL ,
   death_type_concept_id				INTEGER			NOT NULL ,
   cause_concept_id					INTEGER			NULL ,
   cause_source_value					VARCHAR(50)		NULL,
