2363a59799b7b04a320a8ebf016724322beba8eb
clairblacketer <clairblacketer@users.noreply.github.com>
2017-06-21 08:58:43 -0400

    Update MD_to_PDF_conversion.R
    Changed file path to output file so it won't be hidden in the documentation folder.

--- Documentation/MD_to_PDF_conversion.R
+++ Documentation/MD_to_PDF_conversion.R
@@ -1,3 +1,3 @@
 ## Run this code and point it to the .Rmd file so it can convert the markdown files on the wiki to a pdf
 
-rmarkdown::render(input = "C:/Git/CommonDataModel/Documentation/OMOP_CDM_PDF.Rmd", output_format = "pdf_document", output_file = "OMOP_CDM_v5_1_0.pdf")
+rmarkdown::render(input = "C:/Git/CommonDataModel/Documentation/OMOP_CDM_PDF.Rmd", output_format = "pdf_document", output_file = "C:/Git/CommonDataModel/OMOP_CDM_v5_1_0.pdf")
