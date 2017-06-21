fc31a9913c4701504c617029befe5527db4cdfa9
clairblacketer <clairblacketer@users.noreply.github.com>
2017-06-21 09:47:59 -0400

    Remove datetime fields from OBSERVATION_PERIOD

--- Sql Server/OMOP CDM ddl - SQL Server.sql
+++ Sql Server/OMOP CDM ddl - SQL Server.sql
@@ -254,9 +254,7 @@ CREATE TABLE observation_period
   observation_period_id				INTEGER		NOT NULL ,
   person_id							INTEGER		NOT NULL ,
   observation_period_start_date		DATE		NOT NULL ,
-  observation_period_start_datetime		DATETIME2		NOT NULL ,
   observation_period_end_date		DATE		NOT NULL ,
-  observation_period_end_datetime		DATETIME2		NOT NULL ,
   period_type_concept_id				INTEGER		NOT NULL
 )
 ;
