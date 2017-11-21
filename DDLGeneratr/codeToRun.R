70acdf69a69f7ab9c5ea7c31d4e4f01aa9a05e1b
clairblacketer <clair.blacketer@gmail.com>
2017-11-21 08:18:54 -0500

    Fixed .Rmd parameterization

--- DDLGeneratr/codeToRun.R
+++ DDLGeneratr/codeToRun.R
@@ -1,7 +1,13 @@
 #This script is meant to create the OMOP Common Data Model DDLs for each dialect as well as the pdf of the documentation.
 
 
-# Step 1: Update the file inst/sql/sql_server/OMOP CDM ddl.sql with the changes for the new version
+# Step 1: Update the file inst/sql/sql_server/OMOP CDM ddl.sql with the changes for the new version and set the below variables
+
+  # Step 1.1: The version of the CDM you are writing. This will be used for the name of the pdf so, for example, write v5.3 as v5_3
+  cdmVersion <- "v5_3"
+
+  # Step 1.2: The location of the wiki markdown files. The default is "../../Documentation/CommonDataModel_Wiki_Files"
+  mdFilesLocation <- "../../Documentation/CommonDataModel_Wiki_Files"
 
 # Step 2: Run the following code to create the DDLs for each dialect:
 
@@ -30,4 +36,7 @@ writeConstraints("sql server")
 
 # step 5: Run the following code to create the pdf documentation. It will be written to the reports folder.
 
-writePDF(mdFilesLocation = "../../Documentation/CommonDataModel_Wiki_Files", cdmVersion = "v5_3")
+rmarkdown::render("reports/OMOP_CDM_PDF.Rmd",
+                  output_format = "pdf_document",
+                  output_file = paste0("OMOP_CDM_",cdmVersion,".pdf"),
+                  params = list(mdFilesLocation = mdFilesLocation))
