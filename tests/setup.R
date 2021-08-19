639d6a9d0a38567526c3974dbca116875d80bc7d
Adam Black <adam.black@odysseusinc.com>
2021-08-19 07:35:41 -0400

    debug github actions

--- tests/setup.R
+++ tests/setup.R
@@ -1,8 +1,17 @@
 # Download the JDBC drivers used in the tests
 
 # oldJarFolder <- Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
-Sys.setenv("DATABASECONNECTOR_JAR_FOLDER" = file.path(tempdir(check = TRUE), "drivers"))
-DatabaseConnector::downloadJdbcDrivers("postgresql")
+
+driverPath <- file.path(Sys.getenv("HOME"), "drivers")
+if(!dir.exists(driverPath)) dir.create(driverPath)
+Sys.setenv("DATABASECONNECTOR_JAR_FOLDER" = driverPath)
+DatabaseConnector::downloadJdbcDrivers("postgresql", pathToDriver = driverPath)
+
+print(Sys.getenv("DATABASECONNECTOR_JAR_FOLDER"))
+list.files(driverPath)
+# if(Sys.getenv("DATABASECONNECTOR_JAR_FOLDER") == "") {
+  # driverPath <- file.path(Sys.getenv("HOME"), "drivers")
+# }
 # downloadJdbcDrivers("sql server")
 # downloadJdbcDrivers("oracle")
 
