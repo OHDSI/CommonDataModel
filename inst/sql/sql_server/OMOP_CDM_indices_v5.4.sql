867e9bb55910aade386a022d0ec49cc7852a146f
Adam Black <adam.black@odysseusinc.com>
2021-08-18 23:28:57 -0400

    removed spaces in file and folder names. Regenerated ddl output. Tried to fix Field_Level.csv file.

--- inst/sql/sql_server/OMOP_CDM_indices_v5.4.sql
+++ inst/sql/sql_server/OMOP_CDM_indices_v5.4.sql
@@ -1,4 +1,8 @@
-/*@targetdialect OMOP CDM Indices*/
+/*@targetDialect OMOP CDM Indices
+
+  There are no unique indices created because it is assumed that the primary key constraints have been run prior to
+  implementing indices.
+*/
 
 
 /************************
@@ -42,6 +46,8 @@ CREATE INDEX idx_measurement_visit_id_1 ON @cdmDatabaseSchema.measurement (visit
 CREATE CLUSTERED INDEX idx_observation_person_id_1 ON @cdmDatabaseSchema.observation (person_id ASC);
 CREATE INDEX idx_observation_concept_id_1 ON @cdmDatabaseSchema.observation (observation_concept_id ASC);
 CREATE INDEX idx_observation_visit_id_1 ON @cdmDatabaseSchema.observation (visit_occurrence_id ASC);
+
+CREATE CLUSTERED INDEX idx_death_person_id_1 ON @cdmDatabaseSchema.death (person_id ASC);
 
 CREATE CLUSTERED INDEX idx_note_person_id_1 ON @cdmDatabaseSchema.note (person_id ASC);
 CREATE INDEX idx_note_concept_id_1 ON @cdmDatabaseSchema.note (note_type_concept_id ASC);
@@ -142,10 +148,11 @@ CREATE INDEX idx_drug_strength_id_2 ON @cdmDatabaseSchema.drug_strength (ingredi
 
 --Additional v6.0 indices
 
-CREATE CLUSTERED INDEX idx_survey_person_id_1 ON @cdmDatabaseSchema.survey_conduct (person_id ASC);
+--CREATE CLUSTERED INDEX idx_survey_person_id_1 ON @cdmDatabaseSchema.survey_conduct (person_id ASC);
 
-CREATE CLUSTERED INDEX idx_episode_person_id_1 ON @cdmDatabaseSchema.episode (person_id ASC);
-CREATE INDEX idx_episode_concept_id_1 ON @cdmDatabaseSchema.episode (episode_concept_id ASC);
 
-CREATE CLUSTERED INDEX idx_episode_event_id_1 ON @cdmDatabaseSchema.episode_event (episode_id ASC);
-CREATE INDEX idx_ee_field_concept_id_1 ON @cdmDatabaseSchema.episode_event (event_field_concept_id ASC);
+--CREATE CLUSTERED INDEX idx_episode_person_id_1 ON @cdmDatabaseSchema.episode (person_id ASC);
+--CREATE INDEX idx_episode_concept_id_1 ON @cdmDatabaseSchema.episode (episode_concept_id ASC);
+
+--CREATE CLUSTERED INDEX idx_episode_event_id_1 ON @cdmDatabaseSchema.episode_event (episode_id ASC);
+--CREATE INDEX idx_ee_field_concept_id_1 ON @cdmDatabaseSchema.episode_event (event_field_concept_id ASC);
