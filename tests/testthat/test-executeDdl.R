ec02071b64005664868c82068258039aad3770e8
Adam Black <adam.black@odysseusinc.com>
2021-08-19 23:42:04 -0400

    test connection on all platforms

--- tests/testthat/test-executeDdl.R
+++ tests/testthat/test-executeDdl.R
@@ -1,8 +1,8 @@
 # To run tests for one dbms and one cdmVersion use the variables below
 # dbms <- "postgresql"; cdmVersion <- "5.4"
 
-# testDatabases <- c("oracle", "postgresql", "sql server", "redshift")
-testDatabases <- c("redshift")
+testDatabases <- c("oracle", "postgresql", "sql server", "redshift")
+# testDatabases <- c("redshift")
 
 test_that("getConnectionDetails works", {
   for(dbms in testDatabases) {
